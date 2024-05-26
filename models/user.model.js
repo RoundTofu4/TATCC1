61fc53b29290358fbc6db9c7b29fea376814ff56
RoundTofu4 <i.satria.w@gmail.com>
2024-05-26 23:04:00 +0700

    tes8

---
+++ models/user.model.js
@@ -0,0 +1,14 @@
+const db = require('../config/db.config');
+
+const User = {
+  create: (userData, callback) => {
+    const query = 'INSERT INTO users (username, password) VALUES ("?", "?")';
+    db.execute(query, [userData.username, userData.password], callback);
+  },
+  findByUsername: (username, callback) => {
+    const query = 'SELECT * FROM users WHERE username = ?';
+    db.execute(query, [username], callback);
+  }
+};
+
+module.exports = User;
