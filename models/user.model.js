816e1a2ba233d5bb90ee1d4f8a69874371149852
RoundTofu4 <i.satria.w@gmail.com>
2024-05-26 23:23:42 +0700

    9

--- models/user.model.js
+++ models/user.model.js
@@ -2,7 +2,7 @@ const db = require('../config/db.config');
 
 const User = {
   create: (userData, callback) => {
-    const query = 'INSERT INTO users (username, password) VALUES (?, ?)';
+    const query = 'INSERT INTO users (username, password) VALUES ("?", "?")';
     db.execute(query, [userData.username, userData.password], callback);
   },
   findByUsername: (username, callback) => {
