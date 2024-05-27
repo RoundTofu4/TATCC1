0358fce761df935c41c4281cdcb3664e09a1d03c
RoundTofu4 <i.satria.w@gmail.com>
2024-05-27 09:01:22 +0700

    12

--- config/db.config.js
+++ config/db.config.js
@@ -1,10 +1,22 @@
 const mysql = require('mysql2');
 
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+// const db = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+
+const db = mysql.createConnection({
+  host: "34.72.148.145",
+  user: "root",
+  password: "",
+  database: "unix1"
+});
+
+db.connect(function (err) {
+  if (err) throw err;
+  console.log("Database Connected");
 });
 
 module.exports = db;
--- models/user.model.js
+++ models/user.model.js
@@ -2,12 +2,29 @@ const db = require('../config/db.config');
 
 const User = {
   create: (userData, callback) => {
-    const query = 'INSERT INTO users (username, password) VALUES (?, ?)';
-    db.execute(query, [userData.username, userData.password], callback);
+    db.query = ("INSERT INTO users (username, password) VALUES (?, ?)",
+      [userData.username, userData.password],
+      function (err, res) {
+        if (err) {
+          console.log("error: ", err);
+          callback(err, null);
+        }
+      }
+    );
+    // db.execute(query, [userData.username, userData.password], callback);
   },
+  
   findByUsername: (username, callback) => {
-    const query = 'SELECT * FROM users WHERE username = ?';
-    db.execute(query, [username], callback);
+    db.query = ("SELECT * FROM users WHERE username = ?",
+      username, 
+      function (err, res) {
+        if (err) {
+          console.log("error: , err");
+          callback(err, null);
+        }
+      }
+    )
+    // db.execute(query, [username], callback);
   }
 };
 
